d2c5a22183e651f823b66b58d4f33114312a9c23
heilviva <145266521+heilviva@users.noreply.github.com>
2024-06-24 04:45:30 +0300

    Update ExampleInTest.java

--- src/ExampleInTest.java
+++ src/ExampleInTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ExampleInTest {
+package com.example.6practicheskaya;
+import android.content.Context;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInTest {
+    @Test
+    public void useAppContext() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.6practicheskaya", appContext.getPackageName());
+    }
 }
