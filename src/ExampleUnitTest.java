334e3fd0855f0c94d5ba2af263d2deb23fa77934
heilviva <145266521+heilviva@users.noreply.github.com>
2024-06-24 04:44:56 +0300

    Update ExampleUnitTest.java

--- src/ExampleUnitTest.java
+++ src/ExampleUnitTest.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class ExampleUnitTest {
+package com.example.6practicheskaya;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 }
